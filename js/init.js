c0dbd442ec3c6c0bb2045495f03a06716d5f0774
dmerinero <davidmerineroharo@gmail.com>
2017-10-04 23:31:39 +0200

    Generating dinamic divs in 'index' with js

--- js/init.js
+++ js/init.js
@@ -3,5 +3,11 @@
 
     $('.button-collapse').sideNav();
 
+    for (var i = 0; i <20 ; i++) {
+    	$("#products").append('<div class="col s12 m6 l3" style="padding: 0.5em">' +
+    		'				     <div style="background-color: red">iashdgfa</div>' + 
+    						  '</div>')
+    }
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
