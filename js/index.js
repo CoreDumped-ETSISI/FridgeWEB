94d13aa21732d82c4fb7e677b473f8b1c0d3af86
dmerinero <davidmerineroharo@gmail.com>
2017-10-26 21:06:36 +0200

    purchase done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var cart = [];
+
 var settings = {
     "async": true,
     "crossDomain": true,
@@ -6,24 +8,75 @@ var settings = {
     "headers": {}
 };
 
-var cart = [];
+var userData = {
+    "async": true,
+    "crossDomain": true,
+    "url": config.host + "/user",
+    "method": "GET",
+    "headers": {Authorization : 'Bearer ' + localStorage.getItem('token')}
+};
+
+var purchaseSettings = {
+    "async": true,
+    "crossDomain": true,
+    "url": config.host + "/savePurchase",
+    "method": "POST",
+    "headers": {Authorization : 'Bearer ' + localStorage.getItem('token')},
+    "data" : {}
+};
+
+var user;
+
+function getUser(){
+	$.ajax(userData).done(function (response) {
+		user = response;
+		$("#photo").append('<img class="responsive-img circle" src="' + user.avatarImage + '">');
+		$(".name").append(user.displayName);
+		$(".email").append(user.email);
+		updateBalance();
+	});
+}
+
 var productList;
 
+function updateBalance(){
+	$("#saldo").empty();
+	$("#saldo").append(user.balance + ' €');
+}
+
+function purchase(){
+	var asdas = "";
+
+	for(var i = 0; i < cart.length-1; i++)
+		asdas = asdas + cart[i]._id + ",";
+	asdas = asdas + cart[cart.length-1]._id;
+
+	purchaseSettings.data = { productList : asdas};
+
+	console.log(purchaseSettings.data);
+	$.ajax(purchaseSettings).done(function () {
+		getUser();
+	});
+}
 
 $.ajax(settings).done(function (response) {
+	getUser();
+
 	$("#products").empty();
 	
   	for (var i = 0; i < response.length ; i++) {
    		$("#products").append(	'<div class="col s12 m6 xl4" onclick="addToCart(\'' + i + '\')">' +
 					          		'<div class="row" style="padding: 0.5em 0.5em 0em 0.5em; margin: 0px;">' +
-						          		'<div class="card-panel waves-effect waves-green" style="display: inline-block; margin: 0px; width: 100%;">' +
+						          		'<div class="card-panel waves-effect waves-green"' +
+						          			' style="display: inline-block; margin: 0px; width: 100%;">' +
 							            	'<div class="col s4">' +
 								              '<br>' +
 								              '<img class="responsive-img" src="' + response[i].image + '">' +
 								            '</div>' +
 								            '<div class="col s8">' +
 								              '<p style="text-align: center; font-size: 0.9em">' + response[i].name + '</p>' +
-								              '<p style="text-align: left; font-size: 1.2em; margin-left: 1em">' + response[i].price + ' €</p>' +
+								              '<p style="text-align: left; font-size: 1.2em; margin-left: 1em">' + response[i].price + 
+								              	' €</p>' +
 								            '</div>' +
 							        	'</div>' +
 					        	  	'</div>' +
