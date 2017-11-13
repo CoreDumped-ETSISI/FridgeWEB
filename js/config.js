907283fd717ab33dd77242e2d14f08f783e9a64c
dmerinero <davidmerineroharo@gmail.com>
2017-11-13 20:34:44 +0100

    Error in redirection corrected

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 
 var config = {
     "host": function () {
-    			if(window.location.href.indexOf( "http://192.168.0.24" ) > -1 || window.location.href.indexOf( "file" ) > -1)
+    			if(window.location.href.indexOf( "http://192.168.0.27" ) > -1 || window.location.href.indexOf( "file" ) > -1)
     				return "http://192.168.0.24:3000";
     			else
     				return "http://api.coredumped.es";
