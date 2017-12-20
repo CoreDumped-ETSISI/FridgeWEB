87b8b1f37f940d49cc53db96b78d22e879797610
dmerinero <davidmerineroharo@gmail.com>
2017-12-20 17:31:48 +0100

    Restore password functionality done

---
+++ js/forgotPassword.js
@@ -0,0 +1,27 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": config.host() + "/restorePassword",
+    "method": "POST",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded"
+    }
+};
+
+function recover(){
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('#email').val())) 
+    {
+        settings.data = { email : $('#email').val() };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            Materialize.toast('Okey. We will send you a mail to this email ', 4000);
+
+            setTimeout(()=>{ window.location.replace("./login.html") }, 4000);
+        }).fail(function() { Materialize.toast('Something went wrong :(', 4000); });
+    }
+    else{
+        Materialize.toast('Please enter a valid email', 4000)
+    }
+}
---
+++ js/resetPassword.js
@@ -0,0 +1,38 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": config.host() + "/resetPassword/" + collectTokens(),
+    "method": "POST",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded"
+    }
+};
+
+function collectTokens(){
+    var token = window.location.href.split("/");
+    return token[token.length-2] + "/" + token[token.length-1];
+};
+
+function reset(){
+    if($('#rePassword').val() == "" || $('#password').val() == ""){
+        Materialize.toast('Please enter a password', 4000);
+    }
+    else if($('#password').val().length < 8){
+        Materialize.toast('Password must have eight characters', 4000);
+    }
+    else if($('#password').val() == $('#rePassword').val()){
+        settings.data = { password : $('#password').val() };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            Materialize.toast('Password changed', 4000);
+
+            setTimeout(()=>{ window.location.replace("./login.html") }, 4000);
+        }).fail(function() { Materialize.toast('Something went wrong :(', 4000); });
+    }
+    else
+    {
+        Materialize.toast('Both passwords must coincide', 4000);
+    }
+}
