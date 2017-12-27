acdaecf7c3d556ecabc462ae6fbd123fbaeb9fc7
Xavikh <xavikh@gmail.com>
2017-12-27 03:49:11 +0100

    Added verifyEmail page

---
+++ js/verifyEmail.js
@@ -0,0 +1,24 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": config.host() + "/verifyEmail/" + collectTokens(),
+    "method": "POST",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded"
+    }
+};
+
+function collectTokens(){
+    var token = window.location.href.split("=");
+    return token[token.length-1];
+};
+
+function verify(){
+    $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            Materialize.toast('Email verified', 4000);
+
+            setTimeout(()=>{ window.location.replace("./login.html") }, 4000);
+        }).fail(function() { Materialize.toast('Something went wrong :(', 4000); });
+}
